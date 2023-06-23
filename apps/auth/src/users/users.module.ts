29f2a2f8b5b93bb99c7097eb21abd557b70d8a7b
xeusteerapat <xeus085@gmail.com>
2023-06-23 08:26:01 +0700

    :sparkles: Add auth service

---
+++ apps/auth/src/users/users.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UserDocument, UserSchema } from './models/users.schema';
+import { DatabaseModule, LoggerModule } from '@app/common';
+import { UsersRepository } from './users.repository';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      {
+        name: UserDocument.name,
+        schema: UserSchema,
+      },
+    ]),
+    LoggerModule,
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+})
+export class UsersModule {}
